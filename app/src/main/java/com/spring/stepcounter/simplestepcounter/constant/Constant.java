d8836b310bddd9387ae74e9b724ad07a073f908c
hl <dsiyi@qq.com>
2017-11-27 11:13:20 +0800

    Initial commit

---
+++ app/src/main/java/com/spring/stepcounter/simplestepcounter/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.spring.stepcounter.simplestepcounter.constant;
+
+public class Constant {
+    public static final int MSG_FROM_CLIENT = 0;
+    public static final int MSG_FROM_SERVER = 1;
+
+}
